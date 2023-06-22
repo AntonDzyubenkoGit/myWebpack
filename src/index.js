03218411372039b047d87874b702ebbf0fcd7a96
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-06-22 23:11:56 +0300

    Версия сборки #2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-import printMe from './js/print';
-import './js/test';
-import './js/index.ts';
+import './js/test.ts';
 import './style.scss';
+import './img/svg/adversal.svg';
+import './img/svg/anchor.svg';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = 'Hello webpack & Lorem ipsum dolor sit amet.';
-  btn.innerHTML = 'Click me and check the console!!!';
-
-  btn.addEventListener('click', printMe);
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
+console.log('Hi from JS file');
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
     assetModuleFilename: 'assets/[name][ext]',
   },
@@ -20,21 +19,44 @@ module.exports = {
     open: true,
     hot: true,
     port: 9001,
+    watchFiles: path.resolve(__dirname, 'src', 'index.html'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Testing',
+      favicon: path.resolve(__dirname, 'src/img/favicon', 'favicon.ico'),
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new HtmlWebpackPlugin({
+      // title: 'Second page',
+      filename: 'pages/second.[contenthash].html',
+      template: path.resolve(__dirname, 'src/pages', 'second.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+      chunkFilename: '[id].css',
+    }),
+    new ESLintPlugin(),
+  ],
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/,
-        use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.html$/i,
+        use: 'html-loader',
       },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
-      {
-        test: /\.html$/i,
-        use: 'html-loader',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
@@ -43,19 +65,20 @@ module.exports = {
           filename: 'fonts/[name][ext]',
         },
       },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: 'svg-sprite-loader',
+      },
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-      chunkFilename: '[id].css',
-    }),
-    new ESLintPlugin(),
-  ],
 };
